d59b82d7bcf511aea28dc568eba7e8eef0782cd6
Quietiger <mansgbc@gmail.com>
2021-12-08 18:00:24 +0530

    Create basicmath.go

---
+++ basicmath.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(2 + 2)
+	fmt.Println(2 - 2)
+	fmt.Println(2 * 2)
+	fmt.Println(2 / 2)
+	fmt.Println(2 % 2)
+	fmt.Println(2 ^ 2)
+	fmt.Println(2 & 2)
+	fmt.Println(2 | 2)
+	fmt.Println(2 == 2)
+	fmt.Println(2 != 2)
+	fmt.Println(2 < 2)
+	fmt.Println(2 <= 2)
+	fmt.Println(2 > 2)
+	fmt.Println(2 >= 2)
+
+}
