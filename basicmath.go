c19feedb9c5980b0c9e205f6555f539f36d2201d
Quietiger <mansgbc@gmail.com>
2021-12-08 18:14:03 +0530

    Update basicmath.go

--- basicmath.go
+++ basicmath.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(2 + 2)
-	fmt.Println(2 - 2)
-	fmt.Println(2 * 2)
-	fmt.Println(2 / 2)
-	fmt.Println(2 % 2)
+	fmt.Println("Sum =", 2+2)
+	fmt.Println("Difference=", 2-2)
+	fmt.Println("Product=", 2*2)
+	fmt.Println("Reminder=", 2/2)
+	fmt.Println("Modulus=", 2%2)
 	fmt.Println(2 ^ 2)
 	fmt.Println(2 & 2)
 	fmt.Println(2 | 2)
